0b4ef8b77d50c99fca824c33b5cb728ce368c7b2
kwon ho jin <purepeter@anbtech.co.kr>
2014-11-19 22:28:46 +0900

    commit test

--- com.anbtech.anbframe.web/src/test/java/com/anbtech/anbframe/UserServiceTest.java
+++ com.anbtech.anbframe.web/src/test/java/com/anbtech/anbframe/UserServiceTest.java
@@ -50,7 +50,7 @@ public class UserServiceTest {
 		eu.setUserId("anb");
 		eu.setUserName("에이앤비");
 		service.saveOrUpdateUser(eu);
-		
+		// 이거 되는건가? 
 		List<AnbUser> list = service.selectUser(eu);
 		for(AnbUser vo : list){
 			String userId = vo.getUserId();
