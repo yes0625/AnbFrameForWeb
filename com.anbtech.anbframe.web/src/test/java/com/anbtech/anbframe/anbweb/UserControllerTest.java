5f8bb131acf63e8036ed899dfd27e225022814c3
seo jeong hwan <restnfeel@gmail.com>
2014-11-25 22:46:12 +0900

    컨트롤러 테스트 주석달기

--- com.anbtech.anbframe.web/src/test/java/com/anbtech/anbframe/anbweb/UserControllerTest.java
+++ com.anbtech.anbframe.web/src/test/java/com/anbtech/anbframe/anbweb/UserControllerTest.java
@@ -8,17 +8,32 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.web.client.RestTemplate;
 
+// TODO: Auto-generated Javadoc
+/**
+ * 컨트롤러 테스트 작성
+ * 주의사항 : WAS 작동한후 테스트한다.
+ */
 public class UserControllerTest {
 
+	/** The Constant REST_SERVICE_URL. */
 	private static final String REST_SERVICE_URL = "http://localhost:8080/anbweb/";
 	
+	/** The rest template. */
 	private RestTemplate restTemplate;
 	
+	/**
+	 * Inits the.
+	 */
 	@Before
 	public void init(){
 		restTemplate = new RestTemplate();
 	}
 	
+	/**
+	 * Test select.
+	 *
+	 * @throws Exception the exception
+	 */
 	@Test
 	public void testSelect() throws Exception {
 		List list = restTemplate.postForObject(REST_SERVICE_URL+"user_all.do", null, List.class);
