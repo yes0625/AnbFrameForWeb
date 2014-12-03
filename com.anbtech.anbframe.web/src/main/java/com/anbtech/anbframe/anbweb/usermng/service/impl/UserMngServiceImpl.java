5e729aad75455291ae6431e8c709dbfd996e0dd0
seo jeong hwan <restnfeel@gmail.com>
2014-12-04 07:15:41 +0900

    1.잘못된 임포트 제거

--- com.anbtech.anbframe.web/src/main/java/com/anbtech/anbframe/anbweb/usermng/service/impl/UserMngServiceImpl.java
+++ com.anbtech.anbframe.web/src/main/java/com/anbtech/anbframe/anbweb/usermng/service/impl/UserMngServiceImpl.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.anbtech.anbframe.UserServiceTest;
 import com.anbtech.anbframe.anbweb.usermng.dao.UserMngDAO;
 import com.anbtech.anbframe.anbweb.usermng.service.UserMngService;
 import com.anbtech.anbframe.anbweb.usermng.vo.UserMngVO;
