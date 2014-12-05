d7f101ed58becd1f5f1c47e53afa6dd9dc101c01
backham <zinsim007@naver.com>
2014-12-06 00:54:46 +0900

    @Transactional(propagation=Propagation.REQUIRED) 추가

--- com.anbtech.anbframe.web/src/main/java/com/anbtech/anbframe/anbweb/usermng/service/impl/UserMngServiceImpl.java
+++ com.anbtech.anbframe.web/src/main/java/com/anbtech/anbframe/anbweb/usermng/service/impl/UserMngServiceImpl.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.anbtech.anbframe.anbweb.usermng.dao.UserMngDAO;
 import com.anbtech.anbframe.anbweb.usermng.service.UserMngService;
@@ -23,14 +25,17 @@ private static final Logger LOG = LoggerFactory.getLogger(UserMngServiceImpl.cla
 		return userMngDAO.getListUser(param);
 	}
 	
+	@Transactional(propagation=Propagation.REQUIRED)
 	synchronized public int updateUser(UserMngVO param) throws Exception{
 		return userMngDAO.updateUser(param);
 	}
 	
+	@Transactional(propagation=Propagation.REQUIRED)
 	synchronized public int deleteUser(UserMngVO param) throws Exception{
 		return userMngDAO.deleteUser(param);
 	}
 	
+	@Transactional(propagation=Propagation.REQUIRED)
 	synchronized public void inserUser(UserMngVO param) throws Exception{
 		userMngDAO.insertUser(param);
 	}
